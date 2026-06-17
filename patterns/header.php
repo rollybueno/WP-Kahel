9100a4a0098fa47780179262b6b849e911ff2669
Rolly Bueno <rollybueno@gmail.com>
2026-06-17 09:42:26 +0800

    Fix: Header

--- patterns/header.php
+++ patterns/header.php
@@ -8,11 +8,11 @@
  * @since Kahel 1.0
  */
 ?>
-<!-- wp:group {"className":"kahel-main-nav","layout":{"type":"constrained"}} -->
-<div class="wp-block-group kahel-main-nav"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<!-- wp:group {"tagName":"header","metadata":{"patternName":"kahel/header","name":"Header","categories":["header"]},"className":"kahel-main-nav","layout":{"type":"default"}} -->
+<div class="wp-block-group kahel-main-nav"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
 <div class="wp-block-group"><!-- wp:site-logo /-->
 
-<!-- wp:navigation {"ref":2017,"icon":"menu","layout":{"type":"flex","justifyContent":"center"}} /-->
+<!-- wp:navigation {"icon":"menu","layout":{"type":"flex","justifyContent":"center"}} /-->
 
 <!-- wp:buttons -->
 <div class="wp-block-buttons"><!-- wp:button -->
