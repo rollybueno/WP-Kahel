82f59136381496753309c3c2695dcedeea20c48e
Rolly Bueno <rollybueno@gmail.com>
2026-06-17 09:29:53 +0800

    WordPress: Initial theme transformation

---
+++ patterns/header.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Title: Header
+ * Slug: kahel/header
+ * Categories: header
+ *
+ * @subpackage Kahel
+ * @since Kahel 1.0
+ */
+?>
+<!-- wp:group {"className":"kahel-main-nav","layout":{"type":"constrained"}} -->
+<div class="wp-block-group kahel-main-nav"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:site-logo /-->
+
+<!-- wp:navigation {"ref":2017,"icon":"menu","layout":{"type":"flex","justifyContent":"center"}} /-->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Login</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Start Now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
