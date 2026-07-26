393b0d8292529af6bf14eb8e72e8fe175d7468a4
Rolly Bueno <rollybueno@gmail.com>
2026-07-26 22:03:12 +0800

    Wire Kahel single template from preview with theme tokens.

--- functions.php
+++ functions.php
@@ -46,3 +46,46 @@ function kahel_editor_style() {
 	add_editor_style( 'assets/css/main.css' );
 }
 add_action( 'after_setup_theme', 'kahel_editor_style' );
+
+/**
+ * Exclude the current post from related Query Loop blocks.
+ *
+ * Runs against the post-template block; flag via query.kahelExcludeCurrent
+ * or a kahel-related-query className.
+ *
+ * @since 0.1.0
+ *
+ * @param array    $query Array of WP_Query arguments.
+ * @param WP_Block $block The block instance.
+ * @return array
+ */
+function kahel_related_query_exclude_current( $query, $block ) {
+	if ( ! is_singular() ) {
+		return $query;
+	}
+
+	$should_exclude = ! empty( $block->context['query']['kahelExcludeCurrent'] );
+
+	if ( ! $should_exclude ) {
+		$attrs      = is_array( $block->attributes ) ? $block->attributes : array();
+		$class_name = isset( $attrs['className'] ) ? (string) $attrs['className'] : '';
+		$should_exclude = str_contains( $class_name, 'kahel-related-query' );
+	}
+
+	if ( ! $should_exclude ) {
+		return $query;
+	}
+
+	$current_id = (int) get_queried_object_id();
+
+	if ( ! $current_id ) {
+		return $query;
+	}
+
+	$excluded              = isset( $query['post__not_in'] ) ? array_map( 'intval', (array) $query['post__not_in'] ) : array();
+	$excluded[]            = $current_id;
+	$query['post__not_in'] = array_values( array_unique( $excluded ) );
+
+	return $query;
+}
+add_filter( 'query_loop_block_query_vars', 'kahel_related_query_exclude_current', 10, 2 );
